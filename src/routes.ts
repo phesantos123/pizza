9654fcaf53babe234c2fb9bc2fa2f2904e04c0ac
Pedro Henrique <phsantos021@gmail.com>
2022-02-12 23:58:13 -0300

    add routes creat Products

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
+import { CreateProductController } from "./controllers/CreateProductController";
 import { CreateUserController } from "./controllers/CreateUserController";
 
 
 const createUserController = new CreateUserController();
-
+const createProductController = new CreateProductController();
 const routes = Router();
 
 routes.post("/users",createUserController.handle); 
+routes.post("/products",createProductController.handle);
 
 export {routes}
