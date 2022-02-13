e3595b67b63a3b842fc4698fd6d4cd4a72c37f3f
Pedro Henrique <phsantos021@gmail.com>
2022-02-13 00:26:54 -0300

    Ensure Admin

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { CreateProductController } from "./controllers/CreateProductController";
 import { CreateUserController } from "./controllers/CreateUserController";
-
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const createUserController = new CreateUserController();
 const createProductController = new CreateProductController();
 const routes = Router();
 
 routes.post("/users",createUserController.handle); 
-routes.post("/products",createProductController.handle);
+routes.post("/products",ensureAdmin,createProductController.handle);
 
 export {routes}
