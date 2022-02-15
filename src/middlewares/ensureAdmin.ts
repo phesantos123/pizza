450b452da204a20119b892a641340d1e19794caa
Pedro Henrique <phsantos021@gmail.com>
2022-02-14 22:17:04 -0300

    config middlewares

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -3,12 +3,12 @@ import {Request, Response, NextFunction, request} from "express";
 export function ensureAdmin(request: Request, response: Response,next: NextFunction){
 
     //verificar se o User e admin
-    const admin = false;
+    const admin = true;
 
     if(admin){
         return next()
     }
-    
+
     return response.status(401).json({
         error:"User unauthorized"
     });                                                                                                                                                                                                             
